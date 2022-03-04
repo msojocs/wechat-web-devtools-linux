3f7f7a68d5e39bf11a6b4c698c639576d630d8d9
msojocs <jiyecafe@gmail.com>
2022-03-04 10:23:11 +0800

    fix: build version

--- tools/build-deb.sh
+++ tools/build-deb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 root_dir=$(cd `dirname $0`/.. && pwd -P)
-BUILD_VERSION=$1
-
+echo $BUILD_VERSION
+if [ -z "$BUILD_VERSION" ];then
+  export BUILD_VERSION=$1
+fi
+echo $BUILD_VERSION
 cd $root_dir/res/deb
 pwd
 # docker-compose down
