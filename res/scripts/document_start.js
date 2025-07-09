64c51e2d487adaae92db3a3193012a5abf0f1609
msojocs <jiyecafe@gmail.com>
2025-07-09 19:22:23 +0800

    perf: 格式化

--- res/scripts/document_start.js
+++ res/scripts/document_start.js
@@ -1,18 +1,20 @@
 (() => {
     if (!window.skylineRequireReplace) {
-      window.skylineRequireReplace = true
-      const originalRequire = window.require
-      if (originalRequire) {
-        window.require = function (m) {
-          if (m.includes('skyline.node')) {
-            // 给native用的
-            globalThis.__sharedMemory = originalRequire('sharedMemory/sharedMemory.node')
-          }
-          return originalRequire(m)
+        window.skylineRequireReplace = true;
+        const originalRequire = window.require;
+        if (originalRequire) {
+            window.require = function (m) {
+                if (m.includes("skyline.node")) {
+                    // 给native用的
+                    globalThis.__sharedMemory = originalRequire(
+                        "sharedMemory/sharedMemory.node"
+                    );
+                }
+                return originalRequire(m);
+            };
+            window.require.cache = originalRequire.cache;
+            window.require.extensions = originalRequire.extensions;
+            window.require.resolve = originalRequire.resolve;
         }
-        window.require.cache = originalRequire.cache
-        window.require.extensions = originalRequire.extensions
-        window.require.resolve = originalRequire.resolve
-      }
     }
-  })();
+})();
--- res/scripts/entrance.js
+++ res/scripts/entrance.js
@@ -1,11 +1,11 @@
 (() => {
-    const http = require('http')
-    const originaleListen = http.Server.prototype.listen
-    http.Server.prototype.listen = function(port) {
+    const http = require("http");
+    const originaleListen = http.Server.prototype.listen;
+    http.Server.prototype.listen = function (port) {
         if (port == 33233) {
-            console.warn('block port of http server:', port)
-            return
+            console.warn("block port of http server:", port);
+            return;
         }
-        return originaleListen.apply(this, [port])
-    }
+        return originaleListen.apply(this, [port]);
+    };
 })();
