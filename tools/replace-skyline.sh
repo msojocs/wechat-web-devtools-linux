07ee45b403fe30a38d2757ffcff05c4a673c94a1
msojocs <jiyecafe@gmail.com>
2025-11-09 21:00:20 +0800

    feat: Skyline support (#152)
    * feat: document start script
    
    * feat: 修改documentstart
    
    * update: skyline module
    
    * feat: script
    
    * perf: use continuous
    
    * perf: 格式化
    
    * update: skyline version
    
    * feat: skyline sever run script
    
    * docs: skyline使用说明
    
    * docs: CHANGELOG

---
+++ tools/replace-skyline.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+root_dir=$(cd `dirname $0`/.. && pwd -P)
+srcdir=$root_dir
+tmp_dir="$root_dir/tmp"
+cache_dir="$root_dir/cache"
+nwjs_dir="$root_dir/nwjs"
+package_dir="$root_dir/package.nw"
+
+shared_memory_version="v1.0.2"
+skyline_version="v1.0.3"
+
+cd "$package_dir/node_modules"
+rm sharedMemory/sharedMemory.node
+wget -c https://github.com/msojocs/skyline-shared-memory/releases/download/$shared_memory_version/skyline-sharedMemory-linux-x86_64-$shared_memory_version.node -OsharedMemory/sharedMemory.node
+
+cd skyline-addon
+rm build/skyline.node
+wget -c https://github.com/msojocs/skyline-client-server/releases/download/$skyline_version/skyline-skylineClient-linux-x86_64-$skyline_version.node -Obuild/skyline.node
+rm build/icudtl.dat
+rm -rf bundle
+
+mv ${package_dir}/js/extensions/inject/documentstart/index.js ${package_dir}/js/extensions/inject/documentstart/index.js.bak
+cp ${srcdir}/res/scripts/document_start.js ${package_dir}/js/extensions/inject/documentstart/index.js
+cat ${package_dir}/js/extensions/inject/documentstart/index.js.bak >> ${package_dir}/js/extensions/inject/documentstart/index.js
+rm ${package_dir}/js/extensions/inject/documentstart/index.js.bak
---
+++ tools/run-skyline-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker run -d -it \
+  --restart=always \
+  --hostname="$(hostname)" \
+  --env="DISPLAY" \
+  --platform="linux/amd64" \
+  --volume="${XAUTHORITY:-${HOME}/.Xauthority}:/root/.Xauthority:ro" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" \
+  --volume="/dev/shm:/dev/shm" \
+  -p 3001:3001 \
+  --name skyline_server \
+  ghcr.io/msojocs/skyline-client-server:master
