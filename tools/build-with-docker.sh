873f1d265b5cbe998c87885817af3b9c84a99372
msojocs <jiyecafe@gmail.com>
2025-11-09 20:10:47 +0800

    docs: skyline使用说明

--- tools/build-with-docker.sh
+++ tools/build-with-docker.sh
@@ -2,5 +2,10 @@
 
 root_dir=$(cd `dirname $0`/.. && pwd -P)
 echo "$(id -u):$(id -g)"
-docker run -u "$(id -u):$(id -g)" --rm -i -e "ACTION_MODE=${ACTION_MODE:-false}" -w /workspace -v "$root_dir:/workspace" jiyecafe/wechat-devtools-build:v1.0.4 \
+docker run --rm -i \
+    -u "$(id -u):$(id -g)" \
+    -e "ACTION_MODE=${ACTION_MODE:-false}" \
+    -w /workspace \
+    -v "$root_dir:/workspace" \
+    jiyecafe/wechat-devtools-build:v1.0.4 \
     bash ./docker/entrypoint
