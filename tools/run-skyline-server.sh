62f69c338371e3820106931218d3c386e628ee54
msojocs <jiyecafe@gmail.com>
2026-03-29 17:01:40 +0800

    docs: skyline use

--- tools/run-skyline-server.sh
+++ tools/run-skyline-server.sh
@@ -7,9 +7,6 @@ docker run -d \
     --network host \
     -e HOST_UID=$(id -u) \
     -e HOST_GID=$(id -g) \
-    -e XVFB_RES=800x600x24 \
-    -v $HOME/.config/wechat-devtools/WeappPlugin:/workspace/WeappPlugin \
-    -v $root_dir/package.nw/js/ideplugin:/workspace/inspector \
     -v "/dev/shm:/dev/shm" \
     --name wechat_devtools_server \
-    devtools-server:2.01.2510280-1
+    devtools-server:2.01.2510290-1
