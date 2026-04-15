bac0cde4623c79bcf6ebb9d06b86f4b397100c47
msojocs <jiyecafe@gmail.com>
2026-04-15 21:29:05 +0800

    fix: cli端口启动失败 #167

--- res/scripts/entrance.js
+++ res/scripts/entrance.js
@@ -1,11 +1,12 @@
 (() => {
     const http = require("http");
     const originaleListen = http.Server.prototype.listen;
-    http.Server.prototype.listen = function (port) {
+    http.Server.prototype.listen = function (...args) {
         if (port == 33233) {
-            console.warn("block port of http server:", port);
+            const port = args[0];
+            console.warn("[entrance] block port of http server:", port);
             return;
         }
-        return originaleListen.apply(this, [port]);
+        return originaleListen.apply(this, args);
     };
 })();
