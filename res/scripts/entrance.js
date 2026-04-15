a38294845956131abd848edc0675a8faec3ef0c3
msojocs <jiyecafe@gmail.com>
2026-04-15 22:14:30 +0800

    fix: port not defined

--- res/scripts/entrance.js
+++ res/scripts/entrance.js
@@ -2,8 +2,8 @@
     const http = require("http");
     const originaleListen = http.Server.prototype.listen;
     http.Server.prototype.listen = function (...args) {
+        const port = args[0];
         if (port == 33233) {
-            const port = args[0];
             console.warn("[entrance] block port of http server:", port);
             return;
         }
