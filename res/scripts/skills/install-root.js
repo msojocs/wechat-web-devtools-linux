4fdabad73747884745f2c8350c2273591d541109
msojocs <jiyecafe@gmail.com>
2026-09-16 19:57:26 +0800

    fix: 修正skill安装

---
+++ res/scripts/skills/install-root.js
@@ -0,0 +1,22 @@
+
+getInstallRoots = (platform, installRoot) => {
+  if (installRoot) {
+    return [installRoot]
+  }
+  const configDir = `${process.env.HOME}/.config/wechat-devtools/app`
+  if (!fs.existsSync(configDir)) {
+    console.warn('Wechat Devtools config directory does not exist:', configDir)
+    return []
+  }
+  if (!fs.existsSync(`${configDir}/.install_path`)) {
+    console.warn('Wechat Devtools install path file does not exist:', `${configDir}/.install_path`)
+    return []
+  }
+  return [
+    fs.readFileSync(`${configDir}/.install_path`, 'utf8').trim()
+  ]
+}
+getWechatidePath = (platform, installRoot) => {
+  return path.join(installRoot, 'bin', 'wechatide')
+}
+normalizePlatform = () => process.platform
